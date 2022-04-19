43b5edb4b115d0c923d561711232d5944b5b8715
RupakSapkota123 <rupaksapkota2000@gmail.com>
2022-04-19 09:01:23 +0545

    api comments added

--- src/layout/customer/components/Comments.tsx
+++ src/layout/customer/components/Comments.tsx
@@ -1,45 +1,113 @@
-import { Timeline } from 'antd'
+import { Card, Spin, Timeline } from 'antd'
 import moment from 'moment'
 import { ReactHookForm } from 'context/ReactHookForms'
 import React from 'react'
+import { relative } from 'path'
+import TextArea from 'antd/lib/input/TextArea'
+import { Button, FormHelperText } from '@mui/material'
+import { Item } from 'Themes/utilityThemes'
+import { watch } from 'fs'
+import { CustomerContext, CustomerData } from 'context/CustomerContext'
 
 const Comments = () => {
      const [data, setData] = React.useState<any[]>([]);
-     const { register, handleSubmit } = React.useContext(ReactHookForm)
-     // const onSubmit = (data: any) => {
-     //      console.log('data===>', data)
-     //      // setData(data)
-     // }
+     const { Customer, loading } = React.useContext(CustomerData);
 
-     // const onSubmit = React.useCallback(
-     //      (data: any) => {
-     //           data.preventDefault()
-     //           console.log('data===>', data)
-     //           setData(data)
-     //      },
-     //      [data]
-     // );
+     const { register, handleSubmit, reset, control, Controller, watch, errors } = React.useContext(ReactHookForm)
+
+     console.log('Comment', Customer.comments)
+     console.log('Comments', data)
+
+     const onSubmit = (e: any) => {
+          console.log('data', e)
+     }
+
+     React.useEffect(() => {
+          if (Customer) {
+               setData(Customer.comments)
+          }
+     }, [])
+
+     const momentYYMMDD = (date?: any) => moment().format('YYYY-MM-DD')
+     const momentYYMMDDHHMM = (date?: any) => moment().format('HH:mm')
+     const momentAMPM = (date?: any) => moment().format('a')
+
+     console.log()
+
      return (
           <>
-               <form onSubmit={handleSubmit((e: any) => setData([e.name]))}>
-                    <input type="text" {...register("name")} />
-                    <button type="submit">Submit</button>
-               </form>
-               <Timeline mode={
-                    'left'
-               }>
+               <form onSubmit={handleSubmit((e: any) => onSubmit(e))}
+                    style={{
+                         width: '50%',
+                         position: "relative",
+                         top: 50,
+                         left: 100
+                    }}
+               >
                     {
-                         data.map((item: any, idx: number) => (
-                              <>
-                                   <Timeline.Item key={idx}
-                                        //  dot={ <Icon type="idcard" style={{ fontSize: '12px' }} /> }
-                                        color="red">
-                                        {item}
-                                        <span>{moment(item.date).format("DD MMM YYYY")}</span>
-                                   </Timeline.Item>                              </>
-                         ))
+                         data?.length > 0 ? (
+                              <Timeline>
+                                   {
+                                        data?.length === 0 ? (
+                                             <>
+                                                  <div style={{ display: "flex", flexFlow: "column", gap: "5px" }}>
+                                                       <div>
+                                                            <Controller
+                                                                 control={control}
+                                                                 name="comment"
+                                                                 rules={{ required: true }}
+                                                                 render={({ field }: any) => (
+                                                                      <TextArea status={errors.comment ? "error" : ""} showCount maxLength={100} {...field} />
+                                                                 )}
+                                                            />
+                                                            <FormHelperText>(For internal use only)</FormHelperText>
+                                                       </div>
+                                                       <Button type="submit" variant='contained' size="small" sx={{ width: '20%' }}>Submit</Button>
+                                                  </div>
+                                             </>
+                                        ) : (
+                                             <>
+                                                       <Timeline.Item>
+                                                            <Card>
+                                                                 <div style={{ display: "flex", flexFlow: "column", gap: "5px" }}>
+                                                                      <div>
+                                                                           <Controller
+                                                                                control={control}
+                                                                                name="name"
+                                                                                rules={{ required: true }}
+                                                                                render={({ field }: any) => (
+                                                                                     <TextArea showCount maxLength={100} {...field} />
+                                                                                )}
+                                                                           />
+                                                                           <FormHelperText>(For internal use only)</FormHelperText>
+                                                                      </div>
+                                                                      <Button type="submit" variant='contained' size="small" sx={{ width: '20%' }}>Submit</Button>
+                                                                 </div>
+                                                            </Card>
+                                                       </Timeline.Item>
+                                                       {
+                                                            data?.map((item: any, index: number) => (
+                                                                 <Timeline.Item key={index}>
+                                                                      <p>{item.comment}</p>
+                                                                      <Item fontWeight='400'>
+                                                                           {item.createdAt}{' by '}
+                                                                           <Item fontWeight='500'>
+                                                                                {item.createdBy}
+                                                                           </Item>
+                                                                      </Item>
+                                                                 </Timeline.Item>
+                                                            ))
+                                                       }
+                                             </>
+                                        )
+                                   }
+                              </Timeline>
+                         )
+                              : (
+                                   <Spin />
+                              )
                     }
-               </Timeline>
+               </form>
           </>
      )
 }
--- src/layout/customer/components/dist/Comments.js
+++ src/layout/customer/components/dist/Comments.js
@@ -15,31 +15,59 @@ var antd_1 = require("antd");
 var moment_1 = require("moment");
 var ReactHookForms_1 = require("context/ReactHookForms");
 var react_1 = require("react");
+var TextArea_1 = require("antd/lib/input/TextArea");
+var material_1 = require("@mui/material");
+var utilityThemes_1 = require("Themes/utilityThemes");
+var CustomerContext_1 = require("context/CustomerContext");
 var Comments = function () {
     var _a = react_1["default"].useState([]), data = _a[0], setData = _a[1];
-    var _b = react_1["default"].useContext(ReactHookForms_1.ReactHookForm), register = _b.register, handleSubmit = _b.handleSubmit;
-    // const onSubmit = (data: any) => {
-    //      console.log('data===>', data)
-    //      // setData(data)
-    // }
-    // const onSubmit = React.useCallback(
-    //      (data: any) => {
-    //           data.preventDefault()
-    //           console.log('data===>', data)
-    //           setData(data)
-    //      },
-    //      [data]
-    // );
+    var _b = react_1["default"].useContext(CustomerContext_1.CustomerData), Customer = _b.Customer, loading = _b.loading;
+    var _c = react_1["default"].useContext(ReactHookForms_1.ReactHookForm), register = _c.register, handleSubmit = _c.handleSubmit, reset = _c.reset, control = _c.control, Controller = _c.Controller, watch = _c.watch, errors = _c.errors;
+    console.log('Comment', Customer.comments);
+    console.log('Comments', data);
+    var onSubmit = function (e) {
+        console.log('data', e);
+    };
+    react_1["default"].useEffect(function () {
+        if (Customer) {
+            setData(Customer.comments);
+        }
+    }, []);
+    var momentYYMMDD = function (date) { return moment_1["default"]().format('YYYY-MM-DD'); };
+    var momentYYMMDDHHMM = function (date) { return moment_1["default"]().format('HH:mm'); };
+    var momentAMPM = function (date) { return moment_1["default"]().format('a'); };
+    console.log();
     return (react_1["default"].createElement(react_1["default"].Fragment, null,
-        react_1["default"].createElement("form", { onSubmit: handleSubmit(function (e) { return setData([e.name]); }) },
-            react_1["default"].createElement("input", __assign({ type: "text" }, register("name"))),
-            react_1["default"].createElement("button", { type: "submit" }, "Submit")),
-        react_1["default"].createElement(antd_1.Timeline, { mode: 'left' }, data.map(function (item, idx) { return (react_1["default"].createElement(react_1["default"].Fragment, null,
-            react_1["default"].createElement(antd_1.Timeline.Item, { key: idx, 
-                //  dot={ <Icon type="idcard" style={{ fontSize: '12px' }} /> }
-                color: "red" },
-                item,
-                react_1["default"].createElement("span", null, moment_1["default"](item.date).format("DD MMM YYYY"))),
-            "                              ")); }))));
+        react_1["default"].createElement("form", { onSubmit: handleSubmit(function (e) { return onSubmit(e); }), style: {
+                width: '50%',
+                position: "relative",
+                top: 50,
+                left: 100
+            } }, (data === null || data === void 0 ? void 0 : data.length) > 0 ? (react_1["default"].createElement(antd_1.Timeline, null, (data === null || data === void 0 ? void 0 : data.length) === 0 ? (react_1["default"].createElement(react_1["default"].Fragment, null,
+            react_1["default"].createElement("div", { style: { display: "flex", flexFlow: "column", gap: "5px" } },
+                react_1["default"].createElement("div", null,
+                    react_1["default"].createElement(Controller, { control: control, name: "comment", rules: { required: true }, render: function (_a) {
+                            var field = _a.field;
+                            return (react_1["default"].createElement(TextArea_1["default"], __assign({ status: errors.comment ? "error" : "", showCount: true, maxLength: 100 }, field)));
+                        } }),
+                    react_1["default"].createElement(material_1.FormHelperText, null, "(For internal use only)")),
+                react_1["default"].createElement(material_1.Button, { type: "submit", variant: 'contained', size: "small", sx: { width: '20%' } }, "Submit")))) : (react_1["default"].createElement(react_1["default"].Fragment, null,
+            react_1["default"].createElement(antd_1.Timeline.Item, null,
+                react_1["default"].createElement(antd_1.Card, null,
+                    react_1["default"].createElement("div", { style: { display: "flex", flexFlow: "column", gap: "5px" } },
+                        react_1["default"].createElement("div", null,
+                            react_1["default"].createElement(Controller, { control: control, name: "name", rules: { required: true }, render: function (_a) {
+                                    var field = _a.field;
+                                    return (react_1["default"].createElement(TextArea_1["default"], __assign({ showCount: true, maxLength: 100 }, field)));
+                                } }),
+                            react_1["default"].createElement(material_1.FormHelperText, null, "(For internal use only)")),
+                        react_1["default"].createElement(material_1.Button, { type: "submit", variant: 'contained', size: "small", sx: { width: '20%' } }, "Submit")))), data === null || data === void 0 ? void 0 :
+            data.map(function (item, index) { return (react_1["default"].createElement(antd_1.Timeline.Item, { key: index },
+                react_1["default"].createElement("p", null, item.comment),
+                react_1["default"].createElement(utilityThemes_1.Item, { fontWeight: '400' },
+                    item.createdAt,
+                    ' by ',
+                    react_1["default"].createElement(utilityThemes_1.Item, { fontWeight: '500' }, item.createdBy)))); })))))
+            : (react_1["default"].createElement(antd_1.Spin, null)))));
 };
 exports["default"] = Comments;
